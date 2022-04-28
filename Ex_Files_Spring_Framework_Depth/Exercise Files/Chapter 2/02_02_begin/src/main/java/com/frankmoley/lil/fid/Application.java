1a785ea0886ec07d51e686ead1676df75ad1d113
Evgeny <evgeny.rfi@gmail.com>
2022-04-28 05:03:22 +0200

    env variables + provfile tested

--- Ex_Files_Spring_Framework_Depth/Exercise Files/Chapter 2/02_02_begin/src/main/java/com/frankmoley/lil/fid/Application.java
+++ Ex_Files_Spring_Framework_Depth/Exercise Files/Chapter 2/02_02_begin/src/main/java/com/frankmoley/lil/fid/Application.java
@@ -1,5 +1,9 @@
 package com.frankmoley.lil.fid;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.frankmoley.lil.fid.config.ApplicationConfig;
 import com.frankmoley.lil.fid.service.GreetingService;
 import com.frankmoley.lil.fid.service.OutputService;
 import com.frankmoley.lil.fid.service.TimeService;
@@ -7,12 +11,18 @@ import com.frankmoley.lil.fid.service.TimeService;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
+       /* GreetingService greetingService = new GreetingService("Hello");
         TimeService timeService = new TimeService(true);
         OutputService outputService = new OutputService(greetingService, timeService);
-
+        */
+    	
+    	ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+    	OutputService outputService = context.getBean(OutputService.class);
+    	
+    	
+    	
         for (int i=0;i<5;i++){
-            outputService.generateOutput("Frank");
+            outputService.generateOutput();
             Thread.sleep(5000);
         }
     }
--- Ex_Files_Spring_Framework_Depth/Exercise Files/Chapter 2/02_02_begin/src/main/java/com/frankmoley/lil/fid/service/OutputService.java
+++ Ex_Files_Spring_Framework_Depth/Exercise Files/Chapter 2/02_02_begin/src/main/java/com/frankmoley/lil/fid/service/OutputService.java
@@ -4,15 +4,23 @@ public class OutputService {
 
     private final GreetingService greetingService;
     private final TimeService timeService;
+    private final String name;
 
-    public OutputService(GreetingService greetingService, TimeService timeService){
+    public OutputService(GreetingService greetingService, TimeService timeService, String name){
         this.greetingService = greetingService;
         this.timeService = timeService;
+        this.name = name;
     }
-
+/*
     public void generateOutput(String name){
         String output = timeService.getCurrentTime() + " " + greetingService.getGreeting(name);
         System.out.println(output);
     }
+    */
+    public void generateOutput(){
+        String output = timeService.getCurrentTime() + " " + greetingService.getGreeting(name);
+        System.out.println(output);
+    }
+
 
 }
